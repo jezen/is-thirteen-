d5196dc50841f9f7a79d8af0d7b97eae3829dabb
I333245 <rajkumar.selvam@sap.com>
2018-09-19 14:23:12 +0530

    Add Tamil Numeral

--- consts.js
+++ consts.js
@@ -269,6 +269,7 @@ var thirteenStrings = [
     "tlettax", // Maltese
     "tretton", // Swedish
     "பதின்மூன்று", // Tamil
+    "௰௩", // Tamil Numeral
     "สิบสาม", // Thai
     "๑๓", // Thai Numeral
     "SipSam", // Thai Transcription
